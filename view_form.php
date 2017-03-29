98e0c068014292cfbd617c02bd7353155cbaba60
aligate <aligate26@gmail.com>
2017-03-29 19:18:20 +0200

    Update view_form.php

--- view_form.php
+++ view_form.php
@@ -1,10 +1,11 @@
 <h2>Редактирование колонки: <?= htmlspecialchars($field_item['Field']); ?></h2>
 
-   <form action = "myadmin.php" method = 'POST'>
-<p><input type ='text' name='field' size = '25' value ='<?= htmlspecialchars($field_item['Field']); ?>' > <input type ='text' name='type' size = '25' value ='<?= htmlspecialchars($field_item['Type']); ?>' > 
-		<input type ='hidden' name='id' value ='<?= htmlspecialchars($field_item['Field']); ?>' >
-		<input type ='hidden' name='table' value ='<?= htmlspecialchars($table); ?>' >
+ <form action = "myadmin.php" method = 'POST'>
+<p><input type ='text' name='field' size = '25' value ='<?= htmlspecialchars($field_item['Field']); ?>' > 
+<input type ='text' name='type' size = '25' value ='<?= htmlspecialchars($field_item['Type']); ?>' > 
+<input type ='hidden' name='id' value ='<?= htmlspecialchars($field_item['Field']); ?>' >
+<input type ='hidden' name='table' value ='<?= htmlspecialchars($table); ?>' >
 		<br/>
 		<br/>
-		<input type="submit" name="change" value="Cозранить изменения" /></p>
-	</form>
+<input type="submit" name="change" value="Cозранить изменения" /></p>
+</form>
